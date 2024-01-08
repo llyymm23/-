bcf6c939bb22317ef96e7c2056210ecb76d183e6
llyymm23 <jsy98bam1023@naver.com>
2024-01-08 10:48:30 +0900

    포스터 이미지 클릭하면 alert창에 id값 뜸

--- 01.js
+++ 01.js
@@ -1,51 +1,62 @@
 const options = {
     method: 'GET',
     headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxOTUyYjM1M2RlZjU0MzU2MjI5YjAyZDkxZWE5ZjVmZSIsInN1YiI6IjY1OWE1MzU1N2UxMmYwMDI1MzM3YzhjOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.J68Fx1-7kjNt-HrwqVJnCOothbvmu-1wKczIXxPyfLY'
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxOTUyYjM1M2RlZjU0MzU2MjI5YjAyZDkxZWE5ZjVmZSIsInN1YiI6IjY1OWE1MzU1N2UxMmYwMDI1MzM3YzhjOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.J68Fx1-7kjNt-HrwqVJnCOothbvmu-1wKczIXxPyfLY'
     }
-  };
+};
 
-  //영화 정보들 저장하여 카드로 만들 구역 불러옴
-  const movies = document.getElementById("movies");
-  console.log(movies);
+//영화 정보들 저장하여 카드로 만들 구역 불러옴
+const movie = document.getElementById("movie");
 
+fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
+    .then(response => response.json())
+    // api 가져온 값에서 필요한 4가지 요소 골라서 저장 (화살표 함수로)
+    .then((response) => {
+        //필요한 내용 들어있는 results 값들만 따로 저장
+        let results = response["results"];
+        // 다시 results 값들 중 필요한 4가지 요소들(title, overview, poster_path, vote_average) 따로 저장
+        // for문으로 반복문 돌면서 각각 따로 저장, 출력
+        for (let i = 0; i < results.length; i++) {
+            let tt = results[i]["title"];
+            let ov = results[i]["overview"];
+            let pp = results[i]["poster_path"];
+            let va = results[i]["vote_average"];
 
-  fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
-   .then(response => response.json())
-   // api 가져온 값에서 필요한 4가지 요소 골라서 저장 (화살표 함수로)
-   // api 값들 중 필요한 내용 들어있는 results 값들만 따로 저장
-   .then((response) => {
-    let results = response["results"];
-    // 다시 results 값들 중 필요한 4가지 요소들(title, overview, poster_path, vote_average) 따로 저장
-    // for문으로 반복문 돌면서 각각 따로 저장, 출력
-    for(let i=0;i<results.length;i++){
-    let tt = results[i]["title"];
-    let ov = results[i]["overview"];
-    let pp = results[i]["poster_path"];
-    let va = results[i]["vote_average"];
+            //영화 정보 적을 부분 속성 만들기
+            let title = document.createElement('h2');
+            let overview = document.createElement('p');
+            let image = document.createElement('img');
+            let vote_average = document.createElement('p');
 
-    //영화 정보 적을 부분 속성 만들기
-   let title = document.createElement('h2');
-   let overview = document.createElement('p');
-   let image = document.createElement('img');
-   let vote_average = document.createElement('p');
+            // movie 구역 안에 새로운 card라는 구역 만들어 영화 정보 저장
+            const card = document.createElement('div');
+            movie.appendChild(card);
+            card.id = "card";
 
-    //영화 정보들을 만들었던 movies 구역 안에 하위 요소로 추가
-   movies.appendChild(title);
-   movies.appendChild(overview);
-   movies.appendChild(image);
-   movies.appendChild(vote_average);
+            //영화 정보들을 만들었던 card 구역 안에 하위 요소로 추가
+            card.appendChild(title);
+            card.appendChild(overview);
+            card.appendChild(image);
+            card.appendChild(vote_average);
 
-   //내용 추가
-   title.textContent = tt;
-   overview.textContent = ov;
-   image.src = `https://image.tmdb.org/t/p/w500${pp}`;
-   vote_average.textContent = va;
+            //영화 정보 내용 추가
+            title.textContent = tt;
+            overview.textContent = ov;
+            image.src = `https://image.tmdb.org/t/p/w500${pp}`;
+            vote_average.textContent = `영화 평점 : ${va}`;
 
+            //이미지 클릭시 아이디값 나오는 창 만들기
+            //각 아이디 값 저장
+            let id = results[i]["id"];
+            //이미지클릭하면 영화 id값 보여주는 alert창 만들기
+            //화살표 함수 이용
+            const imgclick = () => {
+                alert("영화 id : "+id);
+            }
+            //image에 addEventListener 사용해서 클릭하면 imgclick 행동 일어남
+            image.addEventListener('click',imgclick);
 
-    }
-})
-   .catch(err => console.error(err));
-
-   window.alert("message?");
+        }
+    })
+    .catch(err => console.error(err));
