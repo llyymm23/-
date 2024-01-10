f8bc0a7f1f799ca7f09be9bc40321e6f0713e327
llyymm23 <jsy98bam1023@naver.com>
2024-01-10 15:03:38 +0900

    검색 기능까지 구현 완료.

--- 01.js
+++ 01.js
@@ -18,12 +18,13 @@ fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', opti
         // 다시 results 값들 중 필요한 4가지 요소들(title, overview, poster_path, vote_average) 따로 저장
         // for문으로 반복문 돌면서 각각 따로 저장, 출력
         for (let i = 0; i < results.length; i++) {
+            let id = results[i]["id"];
             let tt = results[i]["title"];
             let ov = results[i]["overview"];
             let pp = results[i]["poster_path"];
             let va = results[i]["vote_average"];
 
-            //영화 정보 적을 부분 속성 만들기
+            // //영화 정보 적을 부분 속성 만들기
             let title = document.createElement('h2');
             let overview = document.createElement('p');
             let image = document.createElement('img');
@@ -34,79 +35,57 @@ fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', opti
             movie.appendChild(card);
             card.id = "card";
 
-            //영화 정보들을 만들었던 card 구역 안에 하위 요소로 추가
-            card.appendChild(title);
-            card.appendChild(overview);
-            card.appendChild(image);
-            card.appendChild(vote_average);
-
-            //영화 정보 내용 추가
+            // //영화 정보 내용 추가
             title.textContent = tt;
             overview.textContent = ov;
             image.src = `https://image.tmdb.org/t/p/w500${pp}`;
             vote_average.textContent = `영화 평점 : ${va}`;
 
+            // //영화 정보들을 만들었던 card 구역 안에 하위 요소로 추가
+            card.appendChild(title);
+            card.appendChild(overview);
+            card.appendChild(image);
+            card.appendChild(vote_average);
+
             //이미지 클릭시 아이디값 나오는 창 만들기
-            //각 아이디 값 저장
-            let id = results[i]["id"];
             //이미지클릭하면 영화 id값 보여주는 alert창 만들기
             //화살표 함수 이용
             const imgclick = () => {
                 alert("영화 id : " + id);
 
             }
+
             //image에 addEventListener 사용해서 클릭하면 imgclick 행동 일어남
             image.addEventListener('click', imgclick);
 
 
-            btn.addEventListener("click", function () {
-                card.style.display = "none"
-                if (tt.toLowerCase().includes(l_input)) {
-                    card.style.display = "block"
+            //검색한 문자값 포함한 영화만 화면에 나오게 하는 함수
+            function search() {
+                //검색한 문자값 input으로 받아와서 저장 
+                const input = document.getElementById("input").value;
+                //입력받은 문자값 모두 소문자로 변환
+                const l_input = input.toLowerCase();
+                //card에 나와있는 영화 제목들 모두 소문자로 변환
+                const l_tt = tt.toLowerCase();
+                //영화 제목에 입력받은 문자값 포함되어 있으면 카드 보여주고
+                //포함되어 있지 않으면 숨김
+                if (l_tt.includes(l_input)) {
+                    card.style.display = "block";
+                } else {
+                    card.style.display = "none";
                 }
+            }
+
+            //영화 찾는 search함수 실행하는 기능 구현할 form 구역 formSearch라는 이름으로 저장
+            const formSearch = document.querySelector("#search");
+            // form 구역 내에 submit이 생기면 search함수 실행하는 이벤트 저장
+            formSearch.addEventListener('submit', function (event) {
+                //검색할 때 계속 처음 상태로 되돌아가지는 상태를 막는 함수
+                event.preventDefault();
+                search();
             })
-
-            // // //입력한 문자값대로 영화 검색하는 함수
-            //  function search (element) {
-            //      //검색한 영화만 보여주기 위해 먼저 영화 카드들 모두 숨김
-            //      document.getElementById(card).style.display = "none";
-            //      //검색 비교할 영화 제목들도 모두 소문자로 변환
-            //      const l_tt = tt.toLowerCase();
-            //      if(l_tt.includes(element)){
-            //          document.getElementById(card).style.display = "block";
-            //      }
-            //  }
-
-
-            //  // //입력한 문자값대로 영화 검색하는 함수
-            //  function search (element) {
-            //     if (element === tt.toLowerCase()) {
-            //         return console.log(element);
-            //     }
-            // }
-
-
-
-
-
-            //  form.addEventListener('submit', function(e) {
-            //      console.log('제출');
-            //  })
-
 
         }
 
     })
     .catch(err => console.error(err));
-
-//검색한 문자값 포함한 영화만 화면에 나오게 하기
-//검색한 문자값 input으로 받아와서 저장
-document.getElementById("btn").onclick = function () {
-    const input = document.getElementById("input").value;
-    //입력받은 문자값 모두 소문자로 변환
-    const l_input = input.toLowerCase();
-    console.log(l_input);
-}
-
-
-
